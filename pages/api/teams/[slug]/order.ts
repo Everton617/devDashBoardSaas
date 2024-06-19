bae8ffede0183383f8d1fd0b6fdf7628931329fb
Bonekazz <juracirdantas@gmail.com>
2024-06-19 18:08:17 -0300

    feat: add Update Order endpoint handler

--- pages/api/teams/[slug]/order.ts
+++ pages/api/teams/[slug]/order.ts
@@ -1,24 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { prisma } from "@/lib/prisma";
+import { throwIfNoTeamAccess} from "models/team";
 
 import { 
-    getCurrentUserWithTeam,
-    getTeam,
-    throwIfNoTeamAccess,
-} from "models/team";
-
-import { 
-    EOrderStatus,
     IOrder,
     createOrder,
     deleteOrder,
-    getOrders
+    getOrders,
+    updateOrder
 } from "models/order";
 
-import { sendAudit } from "@/lib/retraced";
+// import { sendAudit } from "@/lib/retraced";
+// import { throwIfNotAllowed } from "models/user";
 
-import { getCurrentUser, throwIfNotAllowed } from "models/user";
-import { teamSlugSchema, validateWithSchema } from "@/lib/zod";
+import { createOrderSchema, updateOrderSchema, validateWithSchema } from "@/lib/zod";
+import { defaultHeaders } from "@/lib/common";
 
 export default async function handler(
     req: NextApiRequest,
@@ -26,16 +21,18 @@ export default async function handler(
 ) {
 
     try {
-        const { teamId } = await throwIfNoTeamAccess(req, res);
+        const { teamId, user } = await throwIfNoTeamAccess(req, res);
+        console.log(user);
 
         switch (req.method) {
             case "GET":
                 await handleGET(req, res, teamId);
                 break;
             case "POST":
-                await handlePOST(req, res, teamId);
+                await handlePOST(req, res, teamId, user);
                 break;
             case "PUT":
+                await handlePUT(req, res, teamId);
                 break;
             case "DELETE":
                 await handleDELETE(req, res);
@@ -46,28 +43,17 @@ export default async function handler(
         const message = error.message || "Somethin went wrong";
         const status = error.status || 500;
         console.log("cant reach route");
-        console.log("erro-message: ", message);
+        console.log("error-message: ", message);
         res.status(status).json({error: {message}});
     }
 
 }
 
-function validateOrder(order: any) {
-    if (
-        ("status" in order && typeof order.status === "string") &&
-        ("entregador" in order && typeof order.entregador === "string") &&
-        ("rua" in order && typeof order.rua === "string") &&
-        ("numero" in order && typeof order.numero === "string") &&
-        ("complemento" in order && typeof order.complemento === "string") &&
-        ("cep" in order && typeof order.cep === "string") &&
-        ("cidade" in order && typeof order.cidade === "string") &&
-        ("estado" in order && typeof order.estado === "string") &&
-        ("tel" in order && typeof order.tel === "string") &&
-        ("metodo_pag" in order && typeof order.metodo_pag === "string") &&
-        ("instrucoes" in order && typeof order.instrucoes === "string")
-    ) return order;
 
-    throw new Error("Invalid Order");
+async function validateCEPExistence(cep: string) {
+    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`,{headers: defaultHeaders});
+    const data = await response.json();
+    if (data.erro) throw new Error("CEP does not exist!");
 }
 
 async function handleGET(req: NextApiRequest, res: NextApiResponse, teamId: string) {
@@ -81,62 +67,63 @@ async function handleGET(req: NextApiRequest, res: NextApiResponse, teamId: stri
     return res.status(200).json({orders: orders});
 }
 
+const testUpdateOrder = {
+    id: "348452ac-895f-429f-a460-3f72ccc903f8",
+    pedido: "3x pastel de uva",
+    quantidade: 2,
+    status: "ANDAMENTO",
+    entregador: "Marcelo",
+    rua: "Alameda",
+    numero: "12",
+    complemento: "perto dali",
+    cep: "59158-210",
+    cidade: "Natal",
+    estado: "RN",
+    tel: "(84) 98752-2972",
+    metodo_pag: "cartao",
+    instrucoes: "sem tijoloa"
+}
 
-async function handlePOST(req: NextApiRequest, res: NextApiResponse, teamId: string){
+const testNewOrder = {
+    pedido: "3x pastel de uva",
+    quantidade: 2,
+    status: "ANDAMENTO",
+    entregador: "Marcelo",
+    rua: "Alameda",
+    numero: "12",
+    complemento: "perto dali",
+    cep: "59158-210",
+    cidade: "Natal",
+    estado: "RN",
+    tel: "(84) 98752-2972",
+    metodo_pag: "cartao",
+    instrucoes: "sem tijoloa"
+}
+
+const inDev = true;
+
+
+async function handlePOST(
+    req: NextApiRequest,
+    res: NextApiResponse,
+    teamId: string,
+    user: any
+){
 
     // throw if not allowed?
     
     if (!req.body.order) throw new Error("Order not provided");
 
-    // const testOrder = {
-    //     status: "ANDAMENTO",
-    //     entregador: "vini",
-    //     rua: "Alameda",
-    //     numero: "12",
-    //     complemento: "perto dali",
-    //     cep: "532",
-    //     cidade: "Natal",
-    //     estado: "RN",
-    //     tel: "6969",
-    //     metodo_pag: "cartao",
-    //     instrucoes: "sem tijolo"
-   //  }
+    const reqOrder = inDev ? validateWithSchema(createOrderSchema,testNewOrder) : validateWithSchema(createOrderSchema, req.body.order); // validate cep and phone inside
 
-    const { 
-        status,
-        entregador,
-        rua,
-        numero,
-        complemento,
-        cep,
-        cidade,
-        estado,
-        tel,
-        metodo_pag,
-        instrucoes
-     } = validateOrder(req.body.order);
-
-     if (!EOrderStatus[status as keyof typeof EOrderStatus])
-         throw new Error("Invalid Status type");
-
-    // validate cep
-    // validate tel
+     await validateCEPExistence(reqOrder.cep);
 
     const order = {
-       status: status,
+       ...reqOrder,
        horario: new Date(),
-       entregador: entregador,
-       rua: rua,
-       numero: numero,
-       complemento,
-       cep: cep,
-       cidade: cidade,
-       estado: estado,
-       tel: tel,
-       metodo_pag: metodo_pag,
-       instrucoes: instrucoes,
-
-       teamId: teamId
+       createdBy: user.name,
+       teamId: teamId,
+       userId: user.id
     } as IOrder;
 
    const newOrder = await createOrder(order);
@@ -147,13 +134,40 @@ async function handlePOST(req: NextApiRequest, res: NextApiResponse, teamId: str
     //     user: user,
     //     team: team
     //  })
+   //  recordMetric()
     
     console.log("Order created!");
     console.log(newOrder);
-    // console.log(`team name ==> ${team.name}`)
     return res.json({data: newOrder, message: "order created!"});
 
 
+}
+
+async function handlePUT(req: NextApiRequest, res: NextApiResponse, teamId: string) {
+
+    if (!req.body.order) throw new Error("Order not provided");
+
+    const reqOrder = inDev ? validateWithSchema(updateOrderSchema, testUpdateOrder) : 
+         validateWithSchema(updateOrderSchema, req.body.order); 
+
+    const order = {
+       id: reqOrder.id.replace("item-", ""),
+       teamId: teamId
+    } as IOrder;
+
+   const newOrder = await updateOrder(order);
+
+    // sendAudit({
+    //     action: "order.create",
+    //     crud: "c",
+    //     user: user,
+    //     team: team
+    //  })
+   //  recordMetric()
+    
+    console.log("Order updated!");
+    console.log(newOrder);
+    return res.json({message: "order updated successfuly"});
 }
 
 async function handleDELETE(req: NextApiRequest, res: NextApiResponse){
